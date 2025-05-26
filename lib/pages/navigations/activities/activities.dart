009f226c63d9f2a169e9bb3806c62fb50d981d39
unknown <zhiyuansun703@gmail.com>
2025-05-27 00:11:36 +0200

    add timeline

--- lib/pages/navigations/activities/activities.dart
+++ lib/pages/navigations/activities/activities.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart';
 import 'package:purr_time/pages/navigations/activities/components/weeklyCalendar.dart';
+import 'package:timelines_plus/timelines_plus.dart';
 
 class Activities extends StatefulWidget {
   const Activities({super.key});
@@ -26,7 +27,7 @@ class _ActivitiesState extends State<Activities> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              "May 26, 2025",
+              DateFormat("MMMM dd, yyyy").format(DateTime.now()),
               style: TextStyle(
                 fontSize: 18.sp,
                 fontWeight: FontWeight.bold,
@@ -46,9 +47,55 @@ class _ActivitiesState extends State<Activities> {
               ),
             ),
 
-            WeeklyCalendar(
-              initialDate: DateTime.now(),
-              onDateSelected: (date) {},
+            Container(
+              margin: EdgeInsets.only(bottom: 10.h),
+              child: WeeklyCalendar(
+                initialDate: DateTime.now(),
+                onDateSelected: (date) {},
+              ),
+            ),
+
+            Expanded(
+              child: FixedTimeline.tileBuilder(
+                builder: TimelineTileBuilder.connected(
+                  contentsAlign: ContentsAlign.alternating,
+                  contentsBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.only(
+                        left: index % 2 == 0 ? 15.w : 0,
+                        right: index % 2 == 0 ? 0 : 15.w,
+                      ),
+                      child: Container(
+                        margin: EdgeInsets.only(bottom: 10.h),
+                        height: 50.h,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.r),
+                          color: Colors.grey[200],
+                        ),
+                        child: Padding(padding: EdgeInsets.symmetric()),
+                      ),
+                    );
+                  },
+
+                  indicatorBuilder: (context, index) {
+                    return OutlinedDotIndicator(
+                      size: 12.w,
+                      color: Colors.blue,
+                      borderWidth: 1.5.w,
+                    );
+                  },
+
+                  connectorBuilder: (context, index, type) {
+                    return SolidLineConnector(
+                      color: Colors.blue,
+                      space: 1.5.w,
+                      indent: type == ConnectorType.start ? 0 : 4.h,
+                      endIndent: type == ConnectorType.end ? 0 : 4.h,
+                    );
+                  },
+                  itemCount: 3,
+                ),
+              ),
             ),
           ],
         ),
